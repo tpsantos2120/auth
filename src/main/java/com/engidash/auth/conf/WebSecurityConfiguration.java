1553d5556a3b0bc0e510684d111c19b126ba88e4
tpsantos2120 <thiagocroza472@gmail.com>
2022-07-08 07:02:34 +0100

    Add web security and password encoder configuration

---
+++ src/main/java/com/engidash/auth/conf/WebSecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.engidash.auth.conf;
+
+import com.engidash.auth.services.user.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Configure AuthenticationManagerBuilder
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/api/v*/registration/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .authenticationManager(authenticationManager(authenticationManagerBuilder))
+                .formLogin();
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+        return auth.authenticationProvider(daoAuthenticationProvider()).build();
+    }
+}
